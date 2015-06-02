bb2a67227024d3f39c75080fdd99e34e73f0aff9
Hunter Green <hgreen@spsu.edu>
2015-06-01 23:46:05 -0400

    Problems 11 completed, 12 in progress

--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -53,6 +53,14 @@ namespace ProjectEuler
                     Problem10();
                     break;
 
+                case 11:
+                    Problem11();
+                    break;
+
+                case 12:
+                    Problem12();
+                    break;
+
                 default:
                     //Do Nothing
                     break;
@@ -228,29 +236,157 @@ namespace ProjectEuler
             Console.WriteLine(sum);
         }
 
+        //A faster alternative would be to set up a sliding calculation for each row, column, and diagonal, resulting in 6n time instead of n^2 time
+        static void Problem11()
+        {
+            int[,] grid = new int[20, 20] { { 08, 02, 22, 97, 38, 15, 00, 40, 00, 75, 04, 05, 07, 78, 52, 12, 50, 77, 91, 08 }, 
+                                            { 49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 04, 56, 62, 00 },
+                                            { 81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 03, 49, 13, 36, 65 },
+                                            { 52, 70, 95, 23, 04, 60, 11, 42, 69, 24, 68, 56, 01, 32, 56, 71, 37, 02, 36, 91 },
+                                            { 22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80 },
+                                            { 24, 47, 32, 60, 99, 03, 45, 02, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50 },
+                                            { 32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70 },
+                                            { 67, 26, 20, 68, 02, 62, 12, 20, 95, 63, 94, 39, 63, 08, 40, 91, 66, 49, 94, 21 },
+                                            { 24, 55, 58, 05, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72 },
+                                            { 21, 36, 23, 09, 75, 00, 76, 44, 20, 45, 35, 14, 00, 61, 33, 97, 34, 31, 33, 95 },
+                                            { 78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 03, 80, 04, 62, 16, 14, 09, 53, 56, 92 },
+                                            { 16, 39, 05, 42, 96, 35, 31, 47, 55, 58, 88, 24, 00, 17, 54, 24, 36, 29, 85, 57 },
+                                            { 86, 56, 00, 48, 35, 71, 89, 07, 05, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58 },
+                                            { 19, 80, 81, 68, 05, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 04, 89, 55, 40 },
+                                            { 04, 52, 08, 83, 97, 35, 99, 16, 07, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66 },
+                                            { 88, 36, 68, 87, 57, 62, 20, 72, 03, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69 },
+                                            { 04, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 08, 46, 29, 32, 40, 62, 76, 36 },
+                                            { 20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 04, 36, 16 },
+                                            { 20, 73, 35, 29, 78, 31, 90, 01, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 05, 54 },
+                                            { 01, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 01, 89, 19, 67, 48 } };
+
+            int product = 0;
+            int max = 0;
+
+            for(int i = 0; i < 20; i++)
+            {
+                for(int j = 0; j < 20; j++)
+                {
+                    //Calculate forward across the column
+                    if(j+3 < 20)
+                        product = grid[i, j] * grid[i, j + 1] * grid[i, j + 2] * grid[i, j + 3];
+
+                    if (product > max)
+                        max = product;
+
+                    //Calculate down across the rows
+                    if(i+3 < 20)
+                        product = grid[i, j] * grid[i + 1, j] * grid[i + 2, j] * grid[i + 3, j];
+
+                    if (product > max)
+                        max = product;
+
+                    //Calculate along the forward diagonal
+                    if(i+3 < 20 && j+3 < 20)
+                        product = grid[i, j] * grid[i + 1, j + 1] * grid[i + 2, j + 2] * grid[i + 3, j + 3];
+
+                    if (product > max)
+                        max = product;
+
+                    //Calculate along the backward diagonal
+                    if (i + 3 < 20 && j - 3 >= 0)
+                        product = grid[i, j] * grid[i + 1, j - 1] * grid[i + 2, j - 2] * grid[i + 3, j - 3];
+
+                    if (product > max)
+                        max = product;
+                }
+            }
+
+            Console.WriteLine(max);
+        }
+
+        
+        static void Problem12()
+        {
+            int seriesIndex = 1;
+            long factorCount = 1;
+
+            long[] subFactorCounts = new long[2];
+            subFactorCounts[0] = 1;
+            subFactorCounts[1] = 1;
+            
+            while (factorCount <= 500)
+            {
+                //Since each triangle number can be written as n*(n+1)/2, where n is the index, the total number of factors is the factors of n plus the factors of n+1 minus 1
+
+                subFactorCounts[0] = subFactorCounts[1];
+                subFactorCounts[1] = GetDivisorCount(seriesIndex + 1);
+
+                factorCount = subFactorCounts[0] + subFactorCounts[1] - 1;
+            }
+
+            Console.WriteLine(Sum1toN(seriesIndex));
+        }
+
+        static long GetDivisorCount(long n)
+        {
+            //First generate a list of primes to test as potential divisors
+            List<long> primes = GetPrimesUnder(n + 1);
+
+            List<long> divisors = new List<long>();
+            List<long> divisorCounts = new List<long>();
+            
+
+            for(int primeIndex = 1; primeIndex < primes.Count; primeIndex++)
+            {
+                if (n % primes[primeIndex] == 0)
+                {
+                    n /= primes[primeIndex];
+                    if (divisors[divisors.Count - 1] != primes[primeIndex])
+                    {
+                        divisors.Add(primes[primeIndex]);
+                        divisorCounts.Add(1);
+                    }
+                    else
+                        divisorCounts[divisorCounts.Count - 1]++;
+
+                    primeIndex--;
+                }
+            }
+
+            long divisorCount = 1;
+
+            foreach(long l in divisorCounts)
+            {
+                divisorCount *= (l + 1);
+            }
+
+            return divisorCount;
+        }
+
+        static long Sum1toN(long n)
+        {
+            return (n * (n + 1)) / 2;
+        }
+
         static List<long> GetPrimesUnder(long n)
         {
             List<long> primes = new List<long>();
 
-            bool[] sieve = new bool[n];
+            bool[] isPrime = new bool[n];
 
             for (int i = 2; i < n; i++)
-                sieve[i] = true;
+                isPrime[i] = true;
 
-            for (int num = 2; num < n; num++)
+            for (int number = 2; number < n/2; number++)
             {
-                if(sieve[num])
+                if(isPrime[number])
                 {
-                    for (int multiple = num * 2; multiple < n; multiple += num)
+                    for (int multiple = number * 2; multiple < n; multiple += number)
                     {
-                        sieve[multiple] = false;
+                        isPrime[multiple] = false;
                     }
                 }
             }
 
             for(int i = 0; i < n; i++)
             {
-                if (sieve[i])
+                if (isPrime[i])
                     primes.Add(i);
             }
 
